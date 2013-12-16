af82f7ae0c380ae412951fb758b2ba35178c3716
James Kiesel <james.kiesel@gmail.com>
2013-12-16 12:02:07 -0500

    Damn you heroku!

--- config/application.rb
+++ config/application.rb
@@ -20,6 +20,7 @@ module PNPI
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
 
+    config.assets.precompile += ['*.js', '*.css']
     config.autoload_paths += %W(#{config.root}/app/models/abstract)    
     config.autoload_paths += %W(#{config.root}/app/models/resources)
     config.autoload_paths += %W(#{config.root}/app/models/categories)
