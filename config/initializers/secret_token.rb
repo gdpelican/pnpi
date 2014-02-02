f383bdc696dd03266d9f4ae8c9fa779e57065cb6
James Kiesel <james.kiesel@gmail.com>
2014-02-03 10:50:32 +1300

    Use ENV vars

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -9,4 +9,4 @@
 
 # Make sure your secret_key_base is kept private
 # if you're sharing your code publicly.
-PNPI::Application.config.secret_key_base = '9989a3e2a00a0708ad9c0009bc921b7b3a789c461eca0c31ec16bfa8ccc84e1084ca6d1a1a0f435a160fc8fc0692a54c0099e41dc0006ab2e53f0ce1826b8244'
+PNPI::Application.config.secret_key_base = ENV['PNPI_SECRET']
