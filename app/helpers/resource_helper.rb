8b5294fcbb5b38e773430f3488d825a2dd98f7b6
James Kiesel <james.kiesel@gmail.com>
2014-05-13 12:04:03 -0400

    Add back to profile link to child resources

--- app/helpers/resource_helper.rb
+++ app/helpers/resource_helper.rb
@@ -7,6 +7,11 @@ module ResourceHelper
 
   def resource_action(action, resource, admin)
     data = case action
+    when :back then
+      { icon: 'user',
+        text: 'Back to my profile',
+        show: current_user.present? && admin || resource.owners.include?(current_user.person),
+        href: edit_person_path(current_user.person) }
     when :submit then
       submit_action(resource, admin).merge({ show: true })
     when :deactivate
